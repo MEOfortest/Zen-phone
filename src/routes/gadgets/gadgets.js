f3f8936b34dc6f6dda82ac24c264c1e0b30869d3
Zayper <krasinpascha@yandex.ru>
2024-03-15 02:13:02 +0300

    2

--- src/routes/gadgets/gadgets.js
+++ src/routes/gadgets/gadgets.js
@@ -21,13 +21,13 @@ export const gadgets = [
         id: 4,
         preview: 'https://c.dns-shop.ru/thumb/st1/fit/200/200/0aba4e0a999711202b091d92e6e86ca9/007a38f0440a0f364454389b002d7f317411c64f76dcca9bb5578bf1d62ecc4c.jpg.webp',
         title: 'Xiaomi Redmi Note 12',
-        price: 'Цена: 151 199 ₽',
+        price: 'Цена: 15 999 ₽',
     },
     {
         id: 5,
-        preview: 'blob:https://web.telegram.org/aa984bf1-7817-42ff-9a64-c8e06f5e240a',
-        title: 'Что такое фреймворки и какие они бывают',
-        price: 'Цена: 151 199 ₽',
+        preview: 'https://c.dns-shop.ru/thumb/st1/fit/200/200/5934a67b4e7728af5f31bb02ffb73bca/d724c257977ad0dea464f177f095470018e5c2f0f451402c7e5f984c4e56ebf6.jpg.webp',
+        title: 'Samsung Galaxy S23 Ultra',
+        price: 'Цена: 109 999 ₽',
     },
     {
         id: 6,
