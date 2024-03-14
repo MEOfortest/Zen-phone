778daddb0a261f008e12fed9a3aebae472542f07
Zayper <krasinpascha@yandex.ru>
2024-03-14 10:46:19 +0300

    4

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Павел Красин</a>
+        <div class="nav-links">
+            {#each nav as link} 
+            <a href={link.href} class="link">{link.title}</a>
+            {/each}
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <slot></slot>
+</div>
+
+<script>
+    const nav = [
+        {
+         title: "Обо мне",
+         href: "/about",
+        },
+        {
+         title: "Блог",
+         href: "/blog",
+        },
+        {
+         title: "Новости",
+         href: "/news",
+        },
+        {
+         title: "Контакты",
+         href: "/contacts",
+        },
+    ];
+</script>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+
+    .nav-container {
+        border: 2px solid orange;
+        height: 50px;
+        background-color: white;
+    }
+</style>
