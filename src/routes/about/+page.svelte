9c9d6db80eaa80271b2d3e36326e61d1a3a460f8
Zayper <krasinpascha@yandex.ru>
2024-03-14 21:45:05 +0300

    2

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,18 @@
 
 <div class="contacts">
     <p id="text">Связаться со мной:</p>
-    <div class="VK"><a href="https://vk.com/newzayper"><img src="https://user-life.com/uploads/posts/2019-06/1560957172_vk.png" width="110" height="110" alt="ВКонтакте"></a></div>
-    <div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://pngset.com/images/telegram-new-logo-text-symbol-graphics-art-transparent-png-1597903.png" width="110" height="110" alt="Телеграм"></a></div>
+    <div class="VK"><a href="https://vk.com/newzayper"><img src="https://user-life.com/uploads/posts/2019-06/1560957172_vk.png" width="130" height="130" alt="ВКонтакте"></a></div>
+    <div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://pngset.com/images/telegram-new-logo-text-symbol-graphics-art-transparent-png-1597903.png" width="130" height="130" alt="Телеграм"></a></div>
 </div>
+
+<style>
+ .VK, .Telegram {
+    display: inline-block;
+ }
+
+ p {
+    font-size: 25px;
+    color: blue;
+    font-family: cursive;
+ }
+</style>
