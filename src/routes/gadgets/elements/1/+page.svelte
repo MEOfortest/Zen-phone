43016f1bb535e22996961c3a4129b32bdb8e8a3c
Zayper <krasinpascha@yandex.ru>
2024-03-15 20:48:20 +0300

    5

--- src/routes/gadgets/elements/1/+page.svelte
+++ src/routes/gadgets/elements/1/+page.svelte
@@ -59,7 +59,7 @@ p {
 
 button {
   background-color: #4CAF50;
-  border: none;
+  border: 1px solid black;
   color: white;
   padding: 15px 32px;
   text-align: center;
--- src/routes/gadgets/gadgets.js
+++ src/routes/gadgets/gadgets.js
@@ -45,7 +45,7 @@ export const gadgets = [
         id: 8,
         preview: 'https://c.dns-shop.ru/thumb/st4/fit/500/500/6ecffbf38fe9930b4dd4e9e20b50b5fd/3cd66d87d590ef93853a270acdbaa8577d78f177e4aab719ac289098c541ab91.jpg.webp',
         title: 'MSI MEG Trident X2',
-        price: 'Цена: 289 999 ₽',
+        price: 'Цена: 216 499 ₽',
     },
     {
         id: 9,
